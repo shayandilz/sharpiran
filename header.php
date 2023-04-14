d84cc17b7ad87fdafc4f9902c59d618c30c24dbb
korosh <korosh.mpr@gmail.com>
2023-04-14 10:11:19 +0430

    fix login tabs in product modal

--- header.php
+++ header.php
@@ -2,12 +2,13 @@
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="UTF-8">
-    <meta name="keywords" content="Digify website">
-    <meta name="description" content="">
-    <meta name="author" content="EJ">
+    <meta name="keywords" content="<?= get_bloginfo('name'); ?>">
+    <meta name="description" content="<?= get_bloginfo('description'); ?>">
+    <meta name="author" content="<?= get_bloginfo('author'); ?>">
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/x-icon" href="<?= get_field('logo', 'option')['url']; ?>">
     <?php wp_head(); ?>
 </head>
 
